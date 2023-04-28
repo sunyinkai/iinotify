334e040c281623214e97355eb9041fee32f4311d
yinkaisun <yinkaisun@microsoft.com>
2023-04-28 17:04:54 +0800

    refactor startup and add health check

---
+++ iinotify/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace iinotify
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (!env.IsDevelopment())
+            {
+                // Redirect HTTP to HTTPS
+                app.UseHttpsRedirection();
+            }
+            app.UseHealthChecks("/health");
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
