63f0328934b599275fa1692cca9e60c4e75318e4
sunyinkai <1091491336@qq.com>
2023-05-11 17:36:34 +0800

    Use KeyVault to manage openAI credentials (#3)
    Co-authored-by: yinkaisun <yinkaisun@microsoft.com>

---
+++ iinotify/Common/Configuraitons/KeyVaultConfigurations.cs
@@ -0,0 +1,10 @@
+#nullable disable
+
+namespace iinotify.Common.Configuraitons
+{
+    public class KeyVaultConfigurations
+    {
+        public string KeyVaultURI { get; set; }
+        public string ManagedIdentityClientID { get; set; }
+    }
+}
